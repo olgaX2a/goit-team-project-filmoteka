8fc71eb7f74fa755966f00e198ab74e9cb3020a8
olgaX2a <sekretaryova.olga@gmail.com>
2021-06-08 19:15:47 +0300

    upd rendering
    временный файл со всеми функциями

--- src/js/renderingSearch.js
+++ src/js/renderingSearch.js
@@ -1,16 +1,6 @@
 import movieService from '../apiServises/apiService';
 
-// для рендера главной (необходимо дорабатывать)
-function mainPage() {
-    return movieService.fetchTrending()
-}
 // для рендера результатов поиска фильма (необходимо дорабатывать)
 function movieSearchResults() {
     return movieService.searchMovie()
 }
-// для рендера полной информации о фильме (необходимо дорабатывать)
-function movieFullInfo(id) {
-    return movieService.getMovieInfoById(id)
-}
-
-mainPage()
