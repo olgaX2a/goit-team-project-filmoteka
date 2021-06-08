721351195de0cefe83a1192a3074beb4abbd0b46
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-06-08 20:58:33 +0300

    рендер популярных фильмов

---
+++ src/js/genre.js
@@ -0,0 +1,10 @@
+// import genreFilm from '../apiServises/searchGenre';
+// import genre from '../templates/genre';
+
+// const genreList = document.querySelector('.genre__list');
+
+// genreFilm.fetchGenre().then(addGenre);
+
+// function addGenre(genres) {
+//     genreList.insertAdjacentHTML('afterbegin', genre(genres));
+// }
