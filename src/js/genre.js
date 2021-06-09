98133d8b2b4157318fa32cdd812b1cee479c1447
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-06-09 21:10:59 +0300

    добавила жанр

--- src/js/genre.js
+++ src/js/genre.js
@@ -1,10 +1,4 @@
-// import genreFilm from '../apiServises/searchGenre';
-// import genre from '../templates/genre';
+import genreFilm from '../apiServises/searchGenre';
+import genre from '../templates/genre';
 
-// const genreList = document.querySelector('.genre__list');
 
-// genreFilm.fetchGenre().then(addGenre);
-
-// function addGenre(genres) {
-//     genreList.insertAdjacentHTML('afterbegin', genre(genres));
-// }
