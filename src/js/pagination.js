83294cf81d4c907a30e9953a61f45153082ac2f9
IiIymik <sambotemp@gmail.com>
2021-06-10 22:50:36 +0300

    push

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 $('#pagination-container').pagination({
-    dataSource: [1, 2, 3, 4, 5, 6, 7, ... , 195],
+    dataSource: [1, 2, 3, 4, 5, 6, 7,195],
     callback: function(data, pagination) {
         // template method of yourself
         var html = template(data);
