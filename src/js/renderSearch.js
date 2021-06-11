b0a7ef6da0df51f7d9da84d29c9dcdcaa59692ce
olgaX2a <sekretaryova.olga@gmail.com>
2021-06-11 17:01:08 +0300

    render search
    рендер результатов поиска по ключевому слову

--- src/js/renderSearch.js
+++ src/js/renderSearch.js
@@ -1,22 +1,36 @@
 import cardFilm from '../templates/card';
 
 import APi from '../apiServises/apiService';
-import {createObj} from '../apiServises/normalizeResults'
+import { createObj } from '../apiServises/normalizeResults';
 
 const cardList = document.querySelector('.card__list');
+const searchRef = document.getElementById('search');
 
-async function renderSearch() {
-    const trends = await APi.searchMovie().then((data) => {
-      return data.results;
-    });
-    const genres = await APi.getGenresList().then((list) => {
-      return list.genres;
-    });
-    const result = await createObj(trends, genres);
-  // временный вывод в консоль для контроля
-    console.log("result :>> ", result);
-    cardList.insertAdjacentHTML('beforeend', cardFilm(result))
+searchRef.addEventListener('submit', onSearchSubmit);
+
+function onSearchSubmit(event) {
+  event.preventDefault();
+  APi.searchQuery = event.target.query.value.trim();
+  // if (!APi.searchQuery.query) {
+  //     return
+  // }
+  // else {
+  //       APi.resetPage();
+  // renderSearch()
+  // }
+  APi.resetPage();
+  renderSearch();
 }
-
-// временный вызов функции для тестирования работоспособности, в дальнейшем повесить на слушатель
-// renderSearch();
+async function renderSearch() {
+  const trends = await APi.searchMovie().then(data => {
+    return data.results;
+  });
+  const genres = await APi.getGenresList().then(list => {
+    return list.genres;
+  });
+  const result = await createObj(trends, genres);
+  // временный вывод в консоль для контроля
+  console.log('result :>> ', result);
+  cardList.innerHTML = cardFilm(result);
+  APi.incrementPage();
+}
