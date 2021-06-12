c6d30e7fb6702518b70bcbc89bc484d0be739dbe
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-06-12 18:03:17 +0300

    работает

--- src/js/renderTrendingFilms.js
+++ src/js/renderTrendingFilms.js
@@ -5,25 +5,25 @@ import {createObj} from '../apiServises/normalizeResults';
 import loading from '../js/spiner';
 
 const cardList = document.querySelector('.card__list');
+const spinerContainer = document.getElementById('spiner');
+// spinerContainer.classList.add('is-hidden');
 
 async function renderTrending() {
-    const trends = await APi.fetchTrending().then((data) => {
-      return data.results;
-    });
-    const genres = await APi.getGenresList().then((list) => {
-      return list.genres;
-    });
-    const result = await createObj(trends, genres);
+  const trends = await APi.fetchTrending().then(data => {
+    return data.results;
+  });
+  const genres = await APi.getGenresList().then(list => {
+    return list.genres;
+  });
+
+  spinerContainer.classList.add('is-hidden');
+  const result = await createObj(trends, genres);
+
   // временный вывод в консоль для контроля
-    console.log("result :>> ", result);
-    cardList.insertAdjacentHTML('beforeend', cardFilm(result))
+  console.log('result :>> ', result);
+  cardList.insertAdjacentHTML('beforeend', cardFilm(result));
 }
 // временный вызов функции для тестирования работоспособности, в дальнейшем повесить на слушатель
+renderTrending();
 
-window.onload = () => {
-  if (renderTrending()) {
-    loading.spinner.stop();
-    console.log(loading.spinner.stop);
-  }
-};
 
--- src/js/spiner.js
+++ src/js/spiner.js
@@ -14,7 +14,7 @@ import {renderTrending} from '../js/renderTrendingFilms';
     rotate: 0, // The rotation offset
     animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
     direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#ffffff', // CSS color or array of colors
+    color: 'grey', // CSS color or array of colors
     fadeColor: 'transparent', // CSS color or array of colors
     top: '50%', // Top position relative to parent
     left: '50%', // Left position relative to parent
@@ -24,7 +24,7 @@ import {renderTrending} from '../js/renderTrendingFilms';
     position: 'absolute', // Element positioning
   };
   
-  const spinerContainer = document.getElementById('spiner__container');
+  const spinerContainer = document.getElementById('spiner');
   const spinner = new Spinner(opts).spin(spinerContainer);
 
   console.log(spinner);
