23ca738813f806682cbbc285c161e629c87532e6
olgaX2a <sekretaryova.olga@gmail.com>
2021-06-13 00:38:56 +0300

    spinner to search
    добавление спиннера к асинхронным запросам (поиск популярного и по ключевому слову), изменён цвет спиннера и тип кнопки на поиске

--- src/js/renderTrendingFilms.js
+++ src/js/renderTrendingFilms.js
@@ -2,41 +2,55 @@ import cardFilm from '../templates/card';
 
 import APi from '../apiServises/apiService';
 import { createObj } from '../apiServises/normalizeResults';
-import loading from '../js/spiner';
+import {showSpinner, hideSpinner} from '../js/spiner';
+
+const searchRef = document.getElementById('search');
 
 const cardList = document.querySelector('.card__list');
-const spinerContainer = document.getElementById('spiner');
-// spinerContainer.classList.add('is-hidden');
 
 // отрисовка популярных фильмов при загрузке страницы;
 document.addEventListener('DOMContentLoaded', renderTrending);
+
 // отрисовка популярных фильмов при нажатии на логотип;
 const logoLink = document.querySelector('.logo');
+
 logoLink.addEventListener('click', () => {
+  clearQuery();
+  showSpinner();
   APi.resetPage();
   renderTrending();
 });
+
 // отрисовка популярных фильмов при нажатии на кнопу HOME;
 const homeLink = document.querySelector('.link__home');
 homeLink.addEventListener('click', () => {
+  clearQuery();
+  showSpinner();
   APi.resetPage();
   renderTrending();
 });
 
 async function renderTrending() {
-  const trends = await APi.fetchTrending().then(data => {
-    return data.results;
-  });
-  const genres = await APi.getGenresList().then(list => {
-    return list.genres;
-  });
+  try {
+    const trends = await APi.fetchTrending().then(data => {
+      return data.results;
+    });
+    const genres = await APi.getGenresList().then(list => {
+      return list.genres;
+    });
+    const result = await createObj(trends, genres);
 
-  spinerContainer.classList.add('is-hidden');
-  const result = await createObj(trends, genres);
-
-  // временный вывод в консоль для контроля
-  console.log('result :>> ', result);
-  cardList.innerHTML = cardFilm(result);
+    // временный вывод в консоль для контроля
+    console.log('result :>> ', result);
+    cardList.innerHTML = cardFilm(result);
+  } catch (error) {
+    console.log('error :>> ', error);
+  }
+  finally {
+    hideSpinner();
+  }
 }
-// временный вызов функции для тестирования работоспособности, в дальнейшем повесить на слушатель
-// renderTrending();
+
+function clearQuery() {
+  searchRef.query.value = ""
+}
