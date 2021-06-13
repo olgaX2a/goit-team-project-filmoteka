c934b77ddfc06f8ec7050bf3448e033fc3d6a32c
Yaroslav Prokudin <jaroslav.prokudin@gmail.com>
2021-06-13 20:30:47 +0300

    proba

--- src/js/renderMovieInfo.js
+++ src/js/renderMovieInfo.js
@@ -1,6 +1,55 @@
-import movieService from '../apiServises/apiService';
+import modalFilmCard from '../templates/modalFilmCard.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
-// для рендера полной информации о фильме (необходимо дорабатывать)
-function movieFullInfo(id) {
-    return movieService.getMovieInfoById(id)
+const apiKey = 'a6a422d110dec9c7fa9eeee757b6f274';
+
+const cardFilm = document.querySelector('.card__list');
+
+cardFilm.addEventListener('click', openModal);
+
+function fetchOneMovieInfo(movie_id) {
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${apiKey}`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => ({
+      ...data,
+      popularity: data.popularity.toFixed(1),
+    }));
+}
+
+function openModal(e) {
+  e.preventDefault();
+
+  fetchOneMovieInfo(e.target.data.id)
+    .then(data => {
+      if (e.target.nodeName !== 'IMG') return;
+
+      const markup = modalFilmCard(data);
+      const modal = basicLightbox.create(markup);
+
+      modal.show();
+
+      const closeBtn = document.querySelector('.modal-close-btn');
+      closeBtn.addEventListener('click', closeModal);
+
+      window.addEventListener('keydown', closeModalHandler);
+
+      function closeModalHandler(e) {
+        if (e.code === 'Escape') {
+          modal.close();
+          window.removeEventListener('keydown', closeModalHandler);
+        }
+      }
+
+      function closeModal(e) {
+        modal.close();
+        window.removeEventListener('keydown', closeModalHandler);
+      }
+
+    })
+    .then(data => {})
+    .catch(error => {
+      console.log('oops!');
+    });
 }
