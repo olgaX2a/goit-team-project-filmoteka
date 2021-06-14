34c5d829383b19a1ff54fb67ceecccd3e040cca1
IiIymik <sambotemp@gmail.com>
2021-06-14 17:19:10 +0300

    add pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -8,45 +8,41 @@ import {BASE_URL} from '../apiServises/apiService.js';
 
 
 
-$('#pagination-container').pagination({
-    dataSource: function(done) {
-    $.ajax({
-        type: 'GET',
-        url: `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${API.pages}&language=${API.language}`,
-      success: function (response) {
-        let pages = response.total_pages;
-        let result = [];
-        for (let i = 1; i <= pages; i++){
-          result.push(i);
-        }
-        // console.log(result)
-            done(result);
-        }
-    });
-  },
-  locator: 'items',
-  totalNumberLocator: function (response) {
-    // console.log(response)
-  },
-      pageSize: 1,
-			showPageNumbers: true,
-			showPrevious: true,
-			showNext: true,
-      autoHidePrevious: true,
-      autoHideNext: true,
-			showFirstOnEllipsisShow: true,
-			showLastOnEllipsisShow: true,
-  callback: function (data, pagination) {
-    API.pages = Number(data);
-    $('.paginationjs-pages ul').addClass('pagination-container list');;
-    $('.paginationjs-pages li').addClass("pagination-button");
-    if (API.searchQuery === '') {
-      renderTrending();
-    } else {
-      renderSearch();
-    }
-  },
-})
+// $('#pagination-container').pagination({
+//     dataSource: function(done) {
+//     $.ajax({
+//         type: 'GET',
+//         url: `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${API.pages}&language=${API.language}`,
+//       success: function (response) {
+//         let pages = response.total_pages;
+//         let result = [];
+//         for (let i = 1; i <= pages; i++){
+//           result.push(i);
+//         }
+//         // console.log(result)
+//             done(result);
+//         }
+//     });
+//   },
+//       pageSize: 1,
+// 			showPageNumbers: true,
+// 			showPrevious: true,
+// 			showNext: true,
+//       autoHidePrevious: true,
+//       autoHideNext: true,
+// 			showFirstOnEllipsisShow: true,
+// 			showLastOnEllipsisShow: true,
+//   callback: function (data, pagination) {
+//     API.pages = Number(data);
+//     $('.paginationjs-pages ul').addClass('pagination-container list');;
+//     $('.paginationjs-pages li').addClass("pagination-button");
+//     if (API.searchQuery === '') {
+//       renderTrending();
+//     } else {
+//       renderSearch();
+//     }
+//   },
+// })
 
 //  `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${API.pages}&language=${API.language}`,
 //   locator: 'totalNumber',
